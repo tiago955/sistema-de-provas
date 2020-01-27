50ff081cb6d7e6381338b466dffbf5dd3b27c4d1
tiago955 <40118323+tiago955@users.noreply.github.com>
2020-01-27 15:56:53 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import user from './user'
+
+
+class teste extends Component {
+    render () {
+        return (
+            <div>
+                react componente 
+                <user name="tiago"></user>
+
+            </div>
+
+        );
+
+    }
+}
+
+
+export default teste;
