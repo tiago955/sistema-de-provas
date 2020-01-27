adc44c0de6f35cbc53fbed21546821a1bb95166f
tiago955 <40118323+tiago955@users.noreply.github.com>
2020-01-27 15:57:14 -0300

    Create user.js

---
+++ user.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class user extends Component {
+    render () {
+        return (
+            
+            <div>
+                Nome:   {this.props.name}
+            </div>
+
+        );
+
+    }
+}
+
+
+export default user;
